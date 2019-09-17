e2ea31e8ff6025b2ccfec60d805853f7410a3115
Charity Parks <tuppercharity@gmail.com>
2019-09-17 00:01:20 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,9 +1,9 @@
 def square_array(array)
   new_array = []
     array.each do |n|
-      
-  new_array << 
+    new_array << n ** 2
     end
+    new_array 
  end
 
 #def any_method(this_is_the_argument)
